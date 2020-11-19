f11b20b901a594315cb69c5f540a57eb31143217
bbblgmsp <71516034+bbblgmsp@users.noreply.github.com>
2020-11-19 23:50:41 +0100

    Update events.php

--- events.php
+++ events.php
@@ -1,59 +1,110 @@
 <?php namespace ProcessWire; ?>
 
 <div id="content">
-    
-    <section>
-        <h1 class="centered uk-margin-medium uk-margin-bottom-large"><?=$page->title;?></h1>
 
-        <div class="noselect centered uk-margin-medium">
-        
+
+    <h1 class="uk-margin-medium uk-margin-bottom-large centered"><?=$page->title;?></h1>
+
+    <div class="centered noselect">
         <button 
             class="uk-width-1-2@s uk-width-auto@m uk-button-default uk-button switches" 
-            id="todaybutton">Today</button>
-            
-            <button 
-            class="uk-width-1-6@s uk-width-auto@m uk-button-default uk-button switches" 
-            id="past" 
-            uk-tooltip="title: previous month; delay: 500" 
-            uk-icon="icon: triangle-left"></button>
-            
-            <button 
-            class="uk-width-1-6@s uk-width-1-6@m uk-button-default uk-button switches" 
-            id="monthbutton" 
-            uk-tooltip="title: Month overview; delay: 500">Month</button>
-            
-            <button 
-            class="uk-width-1-6@s uk-width-auto@m uk-button-default uk-button switches" 
-            id="future" 
-            uk-tooltip="title: next month; delay: 500" 
-            uk-icon="icon: triangle-right"></button>
+            id="todaybutton">Heute</button>
 
-            <!-- 
-            this needs work, a switch for a month overview (which works already) and a year view. 
-            clicking year-view will ditch the month calendar on the left, expand the organizer layout and list all events in that year in a table layout.
-            Also, it would change the button#monthbutton (above) to button#yearbutton and clicking it would show a year overview of all events of the current year. 
-            Clicking the left/right arrows would then switch the year.
+        <button 
+            class="uk-width-1-2@s uk-width-auto@m uk-button-default uk-button switches" 
+            id="past" uk-tooltip="title:voriger Monat; delay: 1000"><span uk-icon="icon:arrow-left; ratio: 1.5"></span></button>
 
-            <button 
-            class="uk-button switches current" 
-            id="monthviewbutton" 
-            uk-tooltip="title: Month view; delay: 500">Month</button>
-            
-            <button 
-            class="uk-button-default uk-button switches" 
-            id="yearviewbutton" 
-            uk-tooltip="title: Year view; delay: 500">Year</button>
-            -->
+        <button 
+            class="uk-width-1-2@s uk-width-1-6@m uk-button-default uk-button switches active" 
+            id="monthbutton" uk-tooltip="title:Monatsübersicht; delay: 1000">Monat</button>
 
-            
-        </div>
+        <button 
+            class="uk-width-1-2@s uk-width-auto@m uk-button-default uk-button switches" 
+            id="yearbutton" uk-tooltip="title:Jahresübersicht; delay: 1000">Jahr</button>
 
-        <div uk-grid class="uk-margin-large">
+        <button 
+            class="uk-width-1-2@s uk-width-auto@m uk-button-default uk-button switches" 
+            id="future" uk-tooltip="title:nächster Monat; delay: 1000"><span uk-icon="icon:arrow-right; ratio: 1.5"></span></button>
+        
+    </div>
 
-            <div id="calendar" class="noselect centered uk-width-1-1@s uk-width-1-3@m"></div>
-
-            <div id="organizer" class="uk-width-1-1@s uk-width-expand@m"><h2>Events</h2></div>
-
+    <section>
+        
+        <div class="uk-margin-large" uk-grid>
+            <div id="calendar" class="uk-width-1-1@s uk-width-1-3@m centered noselect">
+                <h2 id="monthtitle">mm / yyyy</h2>
+                <table id="monthCalendar">
+                    <tbody>
+                        <tr class="header centered">
+                            <th>Mo</th>
+                            <th>Di</th>
+                            <th>Mi</th>
+                            <th>Do</th>
+                            <th>Fr</th>
+                            <th>Sa</th>
+                            <th>So</th>
+                        </tr>
+                        <tr>
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                        </tr>
+                        <tr>
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                        </tr>
+                        <tr>
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                        </tr>
+                        <tr>
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                        </tr>
+                        <tr>
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                        </tr>
+                        <tr>
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                            <td class="centered">&nbsp;</td>    
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="uk-width-1-1@s uk-width-expand@m">
+                <h2 id="headline">Termine events</h2>
+                <div id="organizer"></div>
+            </div>
         </div>
 
     </section>
